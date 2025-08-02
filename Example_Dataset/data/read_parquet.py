8cf867c98d0b0c516d3ae98bec8a638a9ae3d983
Thomas Vienot <71653817+Viennoiserie@users.noreply.github.com>
2025-08-02 19:27:16 +0200

    Delete Example_Dataset directory

--- Example_Dataset/data/read_parquet.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_parquet("C:/Users/thoma/Documents/Thomas - SSD/LoRA - Fine Tune/Example Dataset/oldbookillustrations-small/data/train-00000-of-00001.parquet")
-print(df.head())
