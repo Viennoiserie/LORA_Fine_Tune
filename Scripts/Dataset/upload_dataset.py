f2e4ad0d7348a005d3eb655fff9fffbf015e45ea
Thomas Vienot <71653817+Viennoiserie@users.noreply.github.com>
2025-10-22 09:06:25 +0200

    Update upload_dataset.py

--- Scripts/Dataset/upload_dataset.py
+++ Scripts/Dataset/upload_dataset.py
@@ -1,21 +1,45 @@
 import os
 import pandas as pd
 
+from dotenv import load_dotenv
 from huggingface_hub import login
 from datasets import Dataset, Image
 
-login(token="")
+# --- HF TOKEN --- #
 
-# Chargement des données
-df = pd.read_parquet("Data/dataset.parquet")
-df["image"] = df["image"].apply(lambda x: os.path.join("Dataset", os.path.basename(x)))
+load_dotenv()
+hf_token = os.getenv("HF_TOKEN")
+login(token=hf_token)
 
-df = df[df["image"].apply(os.path.exists)]
-print(f"{len(df)} images prêtes à être envoyées.")
+# --- Directory Variables --- #
 
-# Conversion en Dataset Hugging Face
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, "..", "..", "Data", "images"))
+
+IMAGE_DIR = os.path.join(BASE_DIR, "character")
+DATASET_PATH = os.path.join(BASE_DIR, "dataset.parquet")
+
+# --- Uploading Data --- #
+
+df = pd.read_parquet(DATASET_PATH)
+
+if "image" not in df.columns:
+    raise ValueError("La colonne 'image' est absente du fichier parquet. Vérifie ton dataset.parquet.")
+
+# Verify file integrity
+df["image"] = df["image"].apply(lambda x: os.path.join(IMAGE_DIR, os.path.basename(x)))
+df["exists"] = df["image"].apply(os.path.exists)
+df = df[df["exists"]]
+
+print(f"\n{len(df)} images prêtes à être envoyées.")
+
+if len(df) == 0:
+    raise ValueError("Aucune image trouvée. Vérifie IMAGE_DIR et dataset.parquet.")
+
+# Convert to Hugging Face Dataset
 dataset = Dataset.from_pandas(df)
 dataset = dataset.cast_column("image", Image())
 
-# Upload vers le Hub
-dataset.push_to_hub("Viennoiserie/SDXL_Dataset")
+# --- Push dataset to HF Hub --- #
+
+dataset.push_to_hub("Viennoiserie/Training_Dataset")
